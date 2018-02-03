544d1451497ade5a8df0e7b570cbb20f3f27ce76
Samuel Li <samli@samuelli.net>
2018-02-04 01:29:10 +1100

    converted Attribute class component to function component.

--- src/components/Attribute.js
+++ src/components/Attribute.js
@@ -1,44 +1,36 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Attribute extends Component {
-  constructor(props) {
-    super(props);
+function Attribute(props) {
+  const attribute = props.attribute;
+  const type = props.type;
+  const onAttributeChange = props.onAttributeChange;
+  const yesId = type + '-' + attribute.name + '-yes',
+    noId = type + '-' + attribute.name + '-no';
 
-    this.props = props;
-    this.attribute = props.attribute;
-    this.type = props.type;
-    this.onAttributeChange = props.onAttributeChange;
+  function onChange(attrType, attrName, e) {
+    onAttributeChange(attrType, attrName, e.target.value);
   }
 
-  onChange(attrType, attrName, e) {
-    this.onAttributeChange(attrType, attrName, e.target.value);
-  }
-
-  render() {
-    const yesId = this.type + '-' + this.attribute.name + '-yes',
-          noId = this.type + '-' + this.attribute.name + '-no';
-
-    return (
-      <tr className="attribute-row row">
-        <td className="attribute-cell col-sm-6">
-          { this.attribute.name }
-        </td>
-        <td className="attribute-cell col-sm-3">
-          <input className="form-check-label" type="radio" name={ this.type + '-' + this.attribute.name } value="yes"
-                 id={ yesId }
-                 onChange={ (e) => this.onChange(this.type, this.attribute.name, e) }
-                 defaultChecked={this.attribute.value}/>
-          <label className="form-check-label" htmlFor={ yesId }>YES</label>
-        </td>
-        <td className="attribute-cell col-sm-3">
-          <input className="form-check-label" type="radio" name={ this.type + '-' + this.attribute.name } value="no"
-                 id={ noId } onChange={ (e) => this.onChange(this.type, this.attribute.name, e) }
-                 defaultChecked={!this.attribute.value}/>
-          <label className="form-check-label" htmlFor={ noId }>NO</label>
-        </td>
-      </tr>
-    )
-  }
+  return (
+    <tr className="attribute-row row">
+      <td className="attribute-cell col-sm-6">
+        { attribute.name }
+      </td>
+      <td className="attribute-cell col-sm-3">
+        <input className="form-check-label" type="radio" name={ type + '-' + attribute.name } value="yes"
+               id={ yesId }
+               onChange={ (e) => onChange(type, attribute.name, e) }
+               defaultChecked={ attribute.value }/>
+        <label className="form-check-label" htmlFor={ yesId }>YES</label>
+      </td>
+      <td className="attribute-cell col-sm-3">
+        <input className="form-check-label" type="radio" name={ type + '-' + attribute.name } value="no"
+               id={ noId } onChange={ (e) => onChange(type, attribute.name, e) }
+               defaultChecked={ !attribute.value }/>
+        <label className="form-check-label" htmlFor={ noId }>NO</label>
+      </td>
+    </tr>
+  )
 }
 
 export default Attribute;
