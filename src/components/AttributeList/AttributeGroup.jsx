480f099d9625b07d23423d8788132532be48e350
Samuel Li <samli@samuelli.net>
2018-02-05 00:36:57 +1100

    converted AttributeGroup to function component.

--- src/components/AttributeList/AttributeGroup.jsx
+++ src/components/AttributeList/AttributeGroup.jsx
@@ -1,43 +1,37 @@
-import React, {Component} from 'react';
-import Attribute from './Attribute.jsx';
+import React from 'react';
+import Attribute from './Attribute';
 import NewAttributeValue from './NewAttributeValue';
 
-class AttributeGroup extends Component {
-  constructor(props) {
-    super(props);
-    this.type = props.type;
-    this.attributes = props.attributes;
-    this.onAttributeChange = props.onAttributeChange;
-    this.onAddNewAttributeFromParent = props.onAddNewAttribute;
-    this._onAddNewAttribute = this._onAddNewAttribute.bind(this);
+function AttributeGroup(props) {
+  const type = props.type;
+  const attributes = props.attributes;
+  const onAttributeChange = props.onAttributeChange;
+  const onAddNewAttributeFromParent = props.onAddNewAttribute;
+
+  function _onAddNewAttribute(attribute) {
+    onAddNewAttributeFromParent(type, attribute);
   }
 
-  _onAddNewAttribute(attribute) {
-    this.onAddNewAttributeFromParent(this.type, attribute);
-  }
+  const attributesContent = attributes.map((attribute, index) => {
+    return <Attribute key={ index } type={ type } attribute={ attribute }
+                      onAttributeChange={ onAttributeChange }/>
+  });
 
-  render() {
-    const attributesContent = this.attributes.map((attribute, index) => {
-      return <Attribute key={ index } type={ this.type } attribute={ attribute }
-                        onAttributeChange={ this.onAttributeChange }/>
-    });
-
-    // make a table with header here
-    return (
-      <table className="table table-striped">
-        <thead>
-        <tr className="row">
-          <th colSpan="3" className="col-sm-12">{ this.type }</th>
-        </tr>
-        </thead>
-        <tbody>
-        { attributesContent }
-        <NewAttributeValue newAttributeIndex={ Object.keys(this.attributes).length }
-                           onAdd={ this._onAddNewAttribute }/>
-        </tbody>
-      </table>
-    )
-  }
+  // make a table with header here
+  return (
+    <table className="table table-striped">
+      <thead>
+      <tr className="row">
+        <th colSpan="3" className="col-sm-12">{ type }</th>
+      </tr>
+      </thead>
+      <tbody>
+      { attributesContent }
+      <NewAttributeValue newAttributeIndex={ Object.keys(attributes).length }
+                         onAdd={ _onAddNewAttribute }/>
+      </tbody>
+    </table>
+  )
 }
 
 export default AttributeGroup;
