27798cc1797b7e73591dc82efd7d9ae05244db4a
Samuel Li <samli@samuelli.net>
2018-02-24 22:23:37 +1100

    added Enzyme because I could not figure out how to test interaction on stateless functional component without it.

--- src/components/Attribute/Attribute.spec.js
+++ src/components/Attribute/Attribute.spec.js
@@ -2,34 +2,31 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Attribute from "./Attribute";
 import AttributeModel from 'models/Attribute';
+import {shallow} from 'enzyme';
 
 describe('Attribute', function () {
-  const attributeModel = new AttributeModel('name', true);
-  const attributeType = 'type';
-  const mockOnAttributeChange = jest.fn();
+  const ATTRIBUTE_MODEL = new AttributeModel('name', true);
+  const ATTRIBUTE_TYPE = 'type';
+  const MOCK_ON_ATTRIBUTE_CHANGE_CB = jest.fn();
   beforeEach(function () {
-    mockOnAttributeChange.mockClear();
+    MOCK_ON_ATTRIBUTE_CHANGE_CB.mockClear();
   });
 
   it('should render properly', function () {
     const tree = renderer
-    .create(<Attribute attribute={ attributeModel } type={ attributeType }
-                       onAttributeChange={ mockOnAttributeChange }/>)
+    .create(<Attribute attribute={ ATTRIBUTE_MODEL } type={ ATTRIBUTE_TYPE }
+                       onAttributeChange={ MOCK_ON_ATTRIBUTE_CHANGE_CB }/>)
     .toJSON();
 
     expect(tree).toMatchSnapshot();
   });
 
   it('should call onAttributeChange() in props when attribute value changes', function () {
-    // const component = renderer.create(<Attribute attribute={ attributeModel } type={ attributeType }
-    //                                              onAttributeChange={ mockOnAttributeChange }/>);
-    // const event = {e: {target: {value: 'no'}}};
-    // let attribute = new Attribute({
-    //   attribute: attributeModel,
-    //   type: attributeType,
-    //   onAttributeChange: mockOnAttributeChange
-    // });
-    //
-    // mockOnAttributeChange.toBeCalledWith('type', 'name', 'no');
+    const wrapper = shallow(<Attribute attribute={ ATTRIBUTE_MODEL } type={ ATTRIBUTE_TYPE }
+                                       onAttributeChange={ MOCK_ON_ATTRIBUTE_CHANGE_CB }/>);
+    const noRadioButton = wrapper.find('input[value="no"]');
+    const event = {target: {value: 'no'}};
+    noRadioButton.simulate('change', event);
+    expect(MOCK_ON_ATTRIBUTE_CHANGE_CB).toBeCalledWith('type', 'name', 'no');
   })
 });
