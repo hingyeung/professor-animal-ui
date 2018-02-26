e52f9c64c092c6b52fc3cbea1b5563258f8af8db
Samuel Li <samli@samuelli.net>
2018-02-26 21:58:39 +1100

    removed unnecessary code.

--- src/models/AttributeDefinition.js
+++ src/models/AttributeDefinition.js
@@ -13,22 +13,6 @@ class AttributeDefinition {
     this.diet = diet;
     this.considerations = considerations;
   }
-
-  // static transformFromSerialisedModel(attrDef) {
-  //   // TODO Changed the structure of attributes.json. Note sure if this class has any value now.
-  //   // return new AttributeDefinition(
-  //   //   attrDef.physical.map((attr) => Attribute.transformFromSerialisedModel(attr)),
-  //   //   attrDef.types.map((attr) => Attribute.transformFromSerialisedModel(attr)),
-  //   //   attrDef.behaviours.map((attr) => Attribute.transformFromSerialisedModel(attr)),
-  //   //   attrDef.possible_behaviours.map((attr) => Attribute.transformFromSerialisedModel(attr)),
-  //   //   attrDef.diet.map((attr) => Attribute.transformFromSerialisedModel(attr)),
-  //   //   attrDef.considerations.map((attr) => Attribute.transformFromSerialisedModel(attr)));
-  //   return attrDef;
-  // }
-
-  // static get DEFAULT() {
-  //   return new AttributeDefinition([], [], [], [], [], []);
-  // }
 }
 
 export default AttributeDefinition;
--- src/services/AttributeDefinitionReaderService.js
+++ src/services/AttributeDefinitionReaderService.js
@@ -1,29 +1,7 @@
-import Attribute from '../models/Attribute';
-import AttributeDefinition from '../models/AttributeDefinition';
-
 class AttributeDefinitionReaderService {
   get readFile() {
     return require('../data/attributes.json');
   }
-
-  // attribute-type attributeMap
-  // {
-  // "physical": [{attributes}, {attributes}],
-  // "behaviour": [{attributes}, {attributes}]
-  // }
-  // _buildAttributeTypeMap(attributeList) {
-  //   let attributeTypeMap = {};
-  //   attributeList.forEach(attribute => {
-  //
-  //     attributeTypeMap[attribute.type] =
-  //       attributeTypeMap[attribute.type] ?
-  //         attributeTypeMap[attribute.type].concat(
-  //           new Attribute(attribute.name, attribute.default)) :
-  //         [new Attribute(attribute.name, attribute.default)];
-  //
-  //   });
-  //   return attributeTypeMap;
-  // }
 }
 
 export default AttributeDefinitionReaderService;
