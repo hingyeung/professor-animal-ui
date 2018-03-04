a8d850b61fb32df8b81af29d1590b841d3ebc6f6
Samuel Li <samli@samuelli.net>
2018-03-04 13:08:11 +1100

    added more tests for AttributeList

--- src/components/AttributeList/AttributeList.spec.js
+++ src/components/AttributeList/AttributeList.spec.js
@@ -61,7 +61,7 @@ describe('AttributeList', function () {
 
   it('should render animal route with animal form when animal definition is loaded', function () {
     const wrapper = mount(
-      <MemoryRouter initialEntries={ [{pathname: "/animal/animalId", key: 'testKey', state: {activeAnimalId: "animalId"}}] }>
+      <MemoryRouter initialEntries={ [{pathname: "/animal/animalId", key: 'testKey'}] }>
         <AttributeList onExport={ MOCK_ON_EXPORT }/>
       </MemoryRouter>
     );
@@ -80,7 +80,7 @@ describe('AttributeList', function () {
 
   it('should call onExport() in props with animalDefinition in current state when export button is clicked', function () {
     const wrapper = mount(
-      <MemoryRouter initialEntries={ [{pathname: "/animal/animalId", key: 'testKey', state: {activeAnimalId: "animalId"}}] }>
+      <MemoryRouter initialEntries={ [{pathname: "/animal/animalId", key: 'testKey'}] }>
         <AttributeList onExport={ MOCK_ON_EXPORT }/>
       </MemoryRouter>
     );
@@ -92,4 +92,39 @@ describe('AttributeList', function () {
     // https://stackoverflow.com/a/47318973
     expect(MOCK_ON_EXPORT).toHaveBeenCalledWith(component.state.animalDefinition);
   });
+
+  it('should show blank form', function () {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={[{pathname: "/new", key: "testKey"}]}>
+        <AttributeList onExport={MOCK_ON_EXPORT}/>
+      </MemoryRouter>
+    );
+
+    expect(wrapper.find(LoadableAnimalList)).toHaveLength(1);
+  });
+
+  it('should call onExport function in props when onExport is called', function () {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={[{pathname: "/new", key: "testKey"}]}>
+        <AttributeList onExport={MOCK_ON_EXPORT}/>
+      </MemoryRouter>
+    );
+
+    wrapper.find(AttributeList).instance().onExport({preventDefault: jest.fn()});
+    expect(MOCK_ON_EXPORT).toHaveBeenCalledTimes(1);
+  });
+
+  it('should update state with the current animal when onFormSubmit is called', function () {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={[{pathname: "/new", key: "testKey"}]}>
+        <AttributeList onExport={MOCK_ON_EXPORT}/>
+      </MemoryRouter>
+    );
+
+    const mockRouteHistory = {push: jest.fn()};
+    const attributeListInstance = wrapper.find(AttributeList).instance();
+    const mockAnimal = {id: "id"};
+    attributeListInstance.onFormSubmit(mockAnimal, mockRouteHistory);
+    expect(attributeListInstance.state.animalDefinition).toEqual({id: mockAnimal});
+  })
 });
